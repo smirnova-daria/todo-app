3c119a79da6d0a9938fff4942e86cea8ae0c0d86
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-11 17:17:26 +0300

    add task list display

--- src/components/AllTasks/AllTasks.tsx
+++ src/components/AllTasks/AllTasks.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectTodos } from '../../redux/todosSlice';
 
 export const AllTasks: React.FC = () => {
+
+	const tasks = useSelector(selectTodos)
+
 	return (
 		<div>
 			AllTasks
+			<div>
+				{tasks?.map(t =>
+					<div>
+						{t.todo} - {t.done ? 'done' : 'to do'}
+					</div>)}
+			</div>
 		</div>
 	)
 }
