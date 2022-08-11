0f575bbb46b15e5804f280b6133a1d1e2a87219c
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-11 18:42:12 +0300

    add feature toggle todo

--- src/components/AllTasks/AllTasks.tsx
+++ src/components/AllTasks/AllTasks.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { selectTodos } from '../../redux/todosSlice';
+import { changeTodoStatus, selectTodos } from '../../redux/todosSlice';
 
 export const AllTasks: React.FC = () => {
 	const tasks = useSelector(selectTodos)
+	const dispatch = useDispatch()
+
+	const handleToggleTask = (id: string) => {
+		dispatch(changeTodoStatus(id))
+	}
 
 	return (
 		<div>
 			AllTasks
 			<div>
 				{tasks?.map(t =>
-					<div key={t.id}>
+					<div key={t.id} onClick={() => { handleToggleTask(t.id) }}>
 						{t.todo} - {t.completed ? 'done' : 'to do'}
 					</div>)}
 			</div>
